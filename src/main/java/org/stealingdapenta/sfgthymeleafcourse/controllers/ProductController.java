b847dfb9f041bad785f6a4ceb5ddeee1c515aba5
TomDescheemaeker <tomdescheemaeker@hotmail.com>
2025-02-17 13:39:58 +0100

    Add product retrieval functionality to ProductController

--- src/main/java/org/stealingdapenta/sfgthymeleafcourse/controllers/ProductController.java
+++ src/main/java/org/stealingdapenta/sfgthymeleafcourse/controllers/ProductController.java
@@ -1,14 +1,29 @@
 package org.stealingdapenta.sfgthymeleafcourse.controllers;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.stealingdapenta.sfgthymeleafcourse.services.ProductService;
 
+@RequiredArgsConstructor
 @Controller
 public class ProductController {
+
+    private final ProductService productService;
 
     @RequestMapping("/product")
     public String getProductPage() {
         return "product";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("product", productService.getProduct(id));
+
+        return productService.getProduct(id).toString();
     }
 }
